a4d79404140a125b406a5d460ab67d50d3918cd4
CopenhagenSR <yuehaicen0228@gmail.com>
2014-02-16 16:38:12 -0800

    map

---
+++ mapchage2/Assets/Scripts/SceneFarm.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneFarm : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI() {
+		if (GUI.Button (new Rect (220, 200, 150, 100), "Main Map")) {
+			Application.LoadLevel("SceneMain");		
+		}
+	}
+}
